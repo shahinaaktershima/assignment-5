0cb03c495dbd5fb97aee5be5309adcedde2989c4
shahinaaktershima <shima421914@gmail.com>
2023-08-20 02:02:42 +0600

    new

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// purchase button disabled when total price <0;
+const btnPurchase=document.getElementById('purchase');
+    let purchase =document.getElementById("total-price").innerText;
+    let value=parseFloat(purchase);
+    if(value<=0){
+        btnPurchase.disabled=true;
+    }
+    else{
+        btnPurchase.disabled=false;
+    }
+    // apply button enabled when total price >200;
+    const btnApply=document.getElementById('Apply');
+   if(value<=200){
+    btnApply.disabled=true;
+   }
+   else{
+    btnApply.disabled=false;
+   }
+// total price of all cards
+function addTotal(){
+    const price =document.getElementById('prices').innerText;
+    
+}
