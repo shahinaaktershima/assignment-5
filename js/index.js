a21637a32cd2e08ba2da021f33df3d20365b17d2
shahinaaktershima <shima421914@gmail.com>
2023-08-20 23:33:03 +0600

    'data'

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+// show the card name into a new div
 function textShow(id){
 const parent=document.getElementById('card-name');
 const count =parent.childElementCount;
@@ -15,7 +15,6 @@ function totalPriceCalculation(id){
     const totalPriceValue =parseFloat(totalPrice.innerText);
 const price =document.getElementById(id).innerText;
 const pricevalue=parseFloat(price);
-console.log(pricevalue);
 const total =totalPriceValue+pricevalue;
 const totalFixed=total.toFixed(2);
 totalPrice.innerText =totalFixed;
@@ -24,19 +23,15 @@ addDisabled();
 }
 // discount section
 document.getElementById('apply').addEventListener('click',function(){
-    console.log('clicked');
     const promoCode=document.getElementById('code').textContent;
-    console.log(promoCode)
     const firstInput=document.getElementById('coupon');
     const input=firstInput.value;
-    console.log(input); 
     if(input===promoCode){
         firstInput.value='';
         const totalPrice=document.getElementById('total-price');
         const remainingTotal=document.getElementById('total');
         const discount=document.getElementById('discount');
         const discountValue=parseFloat(totalPrice.innerText)*(20/100);
-        console.log(discountValue);
         const discountFixed=discountValue.toFixed(2)
         discount.innerText=discountFixed;
         const total=parseFloat(totalPrice.innerText)-discountValue;
